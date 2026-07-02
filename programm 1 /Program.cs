bb91dbe9ad50e173776ac3537390a7009d25446c
Bassam <bassam.sa33@gmail.com>
2026-07-02 13:54:45 +0400

    added new commit task 2

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -6,13 +6,27 @@
         {
          ///// Task 1 ///
          
-         Console.WriteLine("Enter number");
-         int number = int.Parse(Console.ReadLine());
-         for ( int i= number ; i>0; i--)
+         // Console.WriteLine("Enter number");
+         // int number = int.Parse(Console.ReadLine());
+         // for ( int i= number ; i>0; i--)
+         // {
+         //     Console.WriteLine(i);
+         // }
+         // Console.WriteLine("Liftoff!");
+         
+         /// Task 2 ////
+         
+         Console.WriteLine("Enter Positive Number");
+         int numbe= int.Parse(Console.ReadLine());
+         int sum = 0;
+         for (int i = 1; i <= numbe; i++)
          {
-             Console.WriteLine(i);
+            sum += i;
          }
-         Console.WriteLine("Liftoff!");
+         Console.WriteLine("fnal result: " + sum);
+             
+          
+         
         }
     }
 }
