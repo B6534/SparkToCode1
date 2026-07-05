f44ea5e3aa79d448f8fc0acf47a2b499eeb36401
Bassam <bassam.sa33@gmail.com>
2026-07-05 22:38:20 +0400

    task 3

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -7,21 +7,21 @@ class Program
     static void Main(string[] args)
     {
         /// task 1 ////
-        // try
-        // {
-        //     Console.WriteLine("Enter first number:");
-        //     float firstNumber = float.Parse(Console.ReadLine());
-        //     
-        //     Console.WriteLine("Enter second number:");
-        //     float secondNumber = float.Parse(Console.ReadLine());
-        //     
-        //     double result = Math.Abs(secondNumber - firstNumber);
-        //     Console.WriteLine(result);
-        // }
-        // catch (Exception )
-        // {
-        //     Console.WriteLine("error: please type valid number only ");
-        ////}//
+         try
+         {
+             Console.WriteLine("Enter first number:");
+             float firstNumber = float.Parse(Console.ReadLine());
+             
+             Console.WriteLine("Enter second number:");
+             float secondNumber = float.Parse(Console.ReadLine());
+             
+             double result = Math.Abs(secondNumber - firstNumber);
+             Console.WriteLine(result);
+         }
+         catch (Exception )
+         {
+             Console.WriteLine("error: please type valid number only ");
+         }
         
         // Task 2 // 
         
@@ -31,6 +31,17 @@ class Program
         double result2 = Math.Pow(number, 2);
         Console.WriteLine("Squared root: "+ result);
         Console.WriteLine("squard: " + result2);
+        
+        // Task 3 // 
+        
+        Console.WriteLine("Enter your full name:");
+        string fullName = Console.ReadLine();
+        int length = fullName.Length;
+        string upper = fullName.ToUpper();
+        string lower = fullName.ToLower();
+        Console.WriteLine(lower);
+        Console.WriteLine(upper);
+        Console.WriteLine(length);
         
     }
         
