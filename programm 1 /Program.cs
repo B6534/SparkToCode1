7103e5f0abec8e8317a66f6e35f2d151e9c2e3b5
Bassam <bassam.sa33@gmail.com>
2026-07-07 12:21:35 +0400

    edit task one

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -2,22 +2,29 @@
 
 class Program
 {
+    /// Function task 1 //// 
+    public static void PrintWlecome(string name)
+    {
+        Console.WriteLine("Welcome " + name );
+    }
+    
+    
 
     static void Main(string[] args)
     {
         
-        // task 1 ////
+        //// task 1 ////
         Console.WriteLine("Enter your name:");
         string name = Console.ReadLine();
         PrintWlecome(name);
         
+        //// task 2 ///
+        
+        
+        
     }
-    
-    /// Function task 1 //// 
-     public static void PrintWlecome(string name)
-     {
-         Console.WriteLine("Welcome " + name );
-     }
+     
+     
         
 
     }
