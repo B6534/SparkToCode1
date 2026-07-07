60ac5ca0ed3c5ef8dde4f341c075cdd9faa7e626
Bassam <bassam.sa33@gmail.com>
2026-07-07 14:46:33 +0400

    commit task 4

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -17,10 +17,19 @@ class Program
 
 
     //Funtcion task 3 /// 
-    public static double celesiustofahrndheit(double c)
+    // public static double celesiustofahrndheit(double c)
+    // {
+    //     double f = (c * 9 / 5) + 32;
+    //     return f; 
+    // }
+
+    //Function task 4 ///
+    public static void displaymenu()
     {
-        double f = (c * 9 / 5) + 32;
-        return f; 
+        Console.WriteLine("Please select an option");
+        Console.WriteLine("1. start");
+        Console.WriteLine("2. help");
+        Console.WriteLine("3. exit");
     }
     
 
@@ -40,10 +49,16 @@ class Program
         // Console.WriteLine("the square is: "+result);
         
         //// task 3 ///
-        Console.WriteLine("Enter temperature in Celsius:");
-        double celsius = double.Parse(Console.ReadLine());
-        double fahrenheit = celesiustofahrndheit(celsius);
-        Console.WriteLine("temperature  in fihernhit is: " + fahrenheit);
+        // Console.WriteLine("Enter temperature in Celsius:");
+        // double celsius = double.Parse(Console.ReadLine());
+        // double fahrenheit = celesiustofahrndheit(celsius);
+        // Console.WriteLine("temperature  in fihernhit is: " + fahrenheit);
+        
+        //// task 4 ////
+        
+        // displaymenu();
+        
+        
 
     }
     
