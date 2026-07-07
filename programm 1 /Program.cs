f3fb2c0717fc1bd86f76b64d08ab9c95120813b9
Bassam <bassam.sa33@gmail.com>
2026-07-07 12:28:14 +0400

    task 2

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -3,26 +3,35 @@
 class Program
 {
     /// Function task 1 //// 
-    public static void PrintWlecome(string name)
+    // public static void PrintWlecome(string name)
+    // {
+    //     Console.WriteLine("Welcome " + name );
+    // }
+
+    // function task 2 //
+    public static int square(int n)
     {
-        Console.WriteLine("Welcome " + name );
+        int sequarev = n * n;
+        return sequarev;
     }
-    
-    
 
     static void Main(string[] args)
     {
         
         //// task 1 ////
-        Console.WriteLine("Enter your name:");
-        string name = Console.ReadLine();
-        PrintWlecome(name);
+        // Console.WriteLine("Enter your name:");
+        // string name = Console.ReadLine();
+        // PrintWlecome(name);
         
         //// task 2 ///
         
-        
+        Console.WriteLine("Enter a number:");
+        int number= int.Parse(Console.ReadLine());
+        int result = square(number);
+        Console.WriteLine("the square is: "+result);
         
     }
+    
      
      
         
