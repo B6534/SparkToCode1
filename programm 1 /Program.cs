c14a331a8c23ec50854c59e67f1ac175b9148602
Bassam <bassam.sa33@gmail.com>
2026-07-08 17:36:09 +0400

    task 4

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -22,43 +22,57 @@ class Program
          
          //// task 2 //
 
-         // List<string> todo = new List<string>();
+         // List<string>t odo = new List<string>();
          // for (int i = 0; i < 5; i++)
          // {
          //     Console.WriteLine("Enter your list what you want to do:" + (i+1));
          //     string add =  Console.ReadLine();
-         //     todo.Add(add);
+         //     to do.Add(add);
          // } 
          // Console.WriteLine("your list is :");
-         // foreach (string list in todo)
+         // foreach (string list in to do)
          // {
          //     Console.WriteLine(list);
          //     
          // }
          
          //// task 3 //
-         Stack<string> history = new Stack<string>();
-         Console.WriteLine("Enter three website:");
-         for ( int i=0; i<3; i++ )
-         {
-             Console.Write("Website URL: " + (i+1));
-             string websiteURL = Console.ReadLine();
-             history.Push(websiteURL);
-         }
-         if (history.Count == 0)
-         {
-             string remove = history.Pop();
-             Console.WriteLine("back: " + remove);
-         }
-         if (history.Count > 0)
-         {
-             Console.WriteLine("You are now on: " + history.Peek());
-         }
-         else
-         {
-             Console.WriteLine("No more pages in history.");
-         }
+         // Stack<string> history = new Stack<string>();
+         // Console.WriteLine("Enter three website:");
+         // for ( int i=0; i<3; i++ )
+         // {
+         //     Console.Write("Website URL: " + (i+1));
+         //     string websiteURL = Console.ReadLine();
+         //     history.Push(websiteURL);
+         // }
+         // if (history.Count == 0)
+         // {
+         //     string remove = history.Pop();
+         //     Console.WriteLine("back: " + remove);
+         // }
+         // if (history.Count > 0)
+         // {
+         //     Console.WriteLine("You are now on: " + history.Peek());
+         // }
+         // else
+         // {
+         //     Console.WriteLine("No more pages in history.");
+         // }
          
+         //// task 4 //
+         Queue<string>  customers = new Queue<string>();
+         for (int i = 0; i < 3; i++)
+         {
+             Console.WriteLine("Enter customer Name: " + (i+1));
+             string customerName = Console.ReadLine();
+             customers.Enqueue(customerName);
+         }
+
+         foreach (string wating in customers)
+         {
+             Console.WriteLine(" wating list: " + wating);
+             
+         }
 
 
 
