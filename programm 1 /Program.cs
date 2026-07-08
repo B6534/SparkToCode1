a218f68ae7dffdec09f6cce74e9624df3b181aca
Bassam <bassam.sa33@gmail.com>
2026-07-08 17:12:09 +0400

    task 2

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -6,18 +6,35 @@ class Program
     {
          //// task 1 ///
 
-         int[] grade = new int [5];
-         for (int i = 0; i < grade.Length; i++)
-         {
-             Console.Write("Enter grade {0}: ", i + 1);
-             grade[i] = int.Parse(Console.ReadLine());
-         }
-         Console.WriteLine("Student Grades:");
-         foreach (int  g in grade)
-         {
-             Console.WriteLine("grade: " + g);
-         }
+         // int[] grade = new int [5];
+         // for (int i = 0; i < grade.Length; i++)
+         // {
+         //     Console.Write("Enter grade {0}: ", i + 1);
+         //     grade[i] = int.Parse(Console.ReadLine());
+         // }
+         // Console.WriteLine("Student Grades:");
+         // foreach (int  g in grade)
+         // {
+         //     Console.WriteLine("grade: " + g);
+         // }
          
+         //// task 2 //
+
+         List<string> todo = new List<string>();
+         for (int i = 0; i < 5; i++)
+         {
+             Console.WriteLine("Enter your list what you want to do:" + (i+1));
+             string add =  Console.ReadLine();
+             todo.Add(add);
+         } 
+         Console.WriteLine("your list is :");
+         foreach (string list in todo)
+         {
+             Console.WriteLine(list);
+             
+         }
+
+
 
 
     }
