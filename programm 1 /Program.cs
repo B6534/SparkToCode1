624c1946ceaec9ef969af904011c75e9959a5d36
Bassam <bassam.sa33@gmail.com>
2026-07-08 17:28:52 +0400

    task 3

--- programm 1 /Program.cs
+++ programm 1 /Program.cs
@@ -1,4 +1,6 @@
-﻿namespace programm_1;
+﻿using System.Reflection.Metadata;
+
+namespace programm_1;
 
 class Program
 {
@@ -20,19 +22,43 @@ class Program
          
          //// task 2 //
 
-         List<string> todo = new List<string>();
-         for (int i = 0; i < 5; i++)
+         // List<string> todo = new List<string>();
+         // for (int i = 0; i < 5; i++)
+         // {
+         //     Console.WriteLine("Enter your list what you want to do:" + (i+1));
+         //     string add =  Console.ReadLine();
+         //     todo.Add(add);
+         // } 
+         // Console.WriteLine("your list is :");
+         // foreach (string list in todo)
+         // {
+         //     Console.WriteLine(list);
+         //     
+         // }
+         
+         //// task 3 //
+         Stack<string> history = new Stack<string>();
+         Console.WriteLine("Enter three website:");
+         for ( int i=0; i<3; i++ )
          {
-             Console.WriteLine("Enter your list what you want to do:" + (i+1));
-             string add =  Console.ReadLine();
-             todo.Add(add);
-         } 
-         Console.WriteLine("your list is :");
-         foreach (string list in todo)
-         {
-             Console.WriteLine(list);
-             
+             Console.Write("Website URL: " + (i+1));
+             string websiteURL = Console.ReadLine();
+             history.Push(websiteURL);
          }
+         if (history.Count == 0)
+         {
+             string remove = history.Pop();
+             Console.WriteLine("back: " + remove);
+         }
+         if (history.Count > 0)
+         {
+             Console.WriteLine("You are now on: " + history.Peek());
+         }
+         else
+         {
+             Console.WriteLine("No more pages in history.");
+         }
+         
 
 
 
