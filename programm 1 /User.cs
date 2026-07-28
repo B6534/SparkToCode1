7b6699ba8a0ed137b5c2a7431c5e2151f1508b6a
Bassam <bassam.sa33@gmail.com>
2026-07-28 09:43:21 +0400

    added new Entity user and category

---
+++ programm 1 /User.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+namespace programm_1;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public List<Order> Orders { get; set; } = new();
+}
